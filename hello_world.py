4455d92e0e50200dab085ab0bcc2bef140630551
ofaith@byupathway.edu <ofaith@byupathway.edu>
2026-03-01 22:25:02 +0100

    updated hello_world.py to ask for the user's name.

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("Hello World!")
+name = ""
+while name == "":
+    name = input("What is your name? ")
+    if name == "":
+        pass
+    else:
+        print(f"Nice to meet you, {name}.")
+        
