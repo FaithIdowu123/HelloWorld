d3b145fe2f801120b6a2908cd8f829aa89efa0bc
ofaith@byupathway.edu <ofaith@byupathway.edu>
2026-03-04 02:26:19 +0100

    Added comments to code

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,18 @@
+#Prints hello world
 print("Hello World!")
+
+#Variable that stores user name as a string
 name = ""
+
+#Code that prevents the program from moving forward unless a name is given
 while name == "":
+    #Gets the name from the user as a string
     name = input("What is your name? ")
+
+    #Restarts the loop if no name is given
     if name == "":
         pass
     else:
+        #Prints a message with the user's name 
         print(f"Nice to meet you, {name}.")
         
